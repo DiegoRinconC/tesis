ad9573556a1df9b5858f6cc9b1ee90e4a22ff89d
DiegoRinconC <diego84darc@gmail.com>
2018-10-21 07:03:19 -0500

    products version

--- smartmarket/products/models.py
+++ smartmarket/products/models.py
@@ -1,3 +1,50 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class ProductType(models.Model):
+    product_type = models.CharField(max_length=200)
+    modified_date = models.DateField(auto_now=True)
+    modified_by = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+
+    def __str__(self):
+        return self.product_type
+
+
+class SubProduct(models.Model):
+    product_type = models.ForeignKey(ProductType, on_delete=models.CASCADE)
+    sub_product_type = models.CharField(max_length=200)
+    modified_date = models.DateField(auto_now=True)
+    modified_by = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+
+    def __str__(self):
+        return self.sub_product_type
+
+
+class BrandProduct(models.Model):
+    brand = models.CharField(max_length=200)
+    modified_date = models.DateField(auto_now=True)
+    modified_by = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+
+    def __str__(self):
+        return self.brand
+
+
+class MeasureProduct(models.Model):
+    unit_measure = models.CharField(max_length=200)
+    modified_date = models.DateField(auto_now=True)
+    modified_by = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+
+    def __str__(self):
+        return self.unit_measure
+
+
+class Product(models.Model):
+    reference = models.CharField(max_length=200)
+    product_type = models.ForeignKey(ProductType, on_delete=models.DO_NOTHING, null=True, blank=True)
+    sub_product_type = models.ForeignKey(SubProduct, on_delete=models.DO_NOTHING, null=True, blank=True)
+    weight_unit = models.DecimalField(max_digits=20, decimal_places=2)
+    unit_measure = models.ForeignKey(MeasureProduct, on_delete=models.DO_NOTHING, null=True, blank=True)
+    quantity = models.IntegerField(null=True, blank=True)

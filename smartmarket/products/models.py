e6f947c3495d0d8ca83f90c6105764c162a0ec2f
DiegoRinconC <diego84darc@gmail.com>
2018-10-21 07:38:19 -0500

    version products y shops

--- smartmarket/products/models.py
+++ smartmarket/products/models.py
@@ -48,3 +48,8 @@ class Product(models.Model):
     weight_unit = models.DecimalField(max_digits=20, decimal_places=2)
     unit_measure = models.ForeignKey(MeasureProduct, on_delete=models.DO_NOTHING, null=True, blank=True)
     quantity = models.IntegerField(null=True, blank=True)
+    modified_date = models.DateField(auto_now=True)
+    modified_by = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+
+    def __str__(self):
+        return self.reference + " " + self.product_type + " " + self.sub_product_type + " " + self.weight_unit + " " + self.unit_measure + " " + self.quantity
--- smartmarket/shops/models.py
+++ smartmarket/shops/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class BrandShop(models.Model):
+    brand_shop = models.CharField(max_length=200)
+    modified_date = models.DateField(auto_now=True)
+    modified_by = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+
+    def __str__(self):
+        return self.brand_shop
+
+
+class Shop(models.Model):
+    shop = models.CharField(max_length=200)
+    brand_shop = models.ForeignKey(BrandShop, on_delete=models.DO_NOTHING, null=True, blank=True)
+    modified_date = models.DateField(auto_now=True)
+    modified_by = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+
+    def __str__(self):
+        return self.shop
